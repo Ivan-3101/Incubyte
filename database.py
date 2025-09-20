e07150aedf4b0a07d06c3c1fe844aef0f9eea898
ivan <ivanad3101@gmail.com>
2025-09-20 16:33:53 +0530

    updated database.py for deployment purpose

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ivands3101$@localhost/sweetshop"
+# This code tells your app:
+# 1. On Render, use the DATABASE_URL environment variable.
+# 2. On your local machine, use the localhost URL.
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL", 
+    "postgresql://postgres:ivands3101$@localhost/sweetshop"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
